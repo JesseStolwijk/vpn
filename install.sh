0c3fb3149b5d69d9cfb2cceef8905f84291fc19a
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-01-25 18:28:50 +0100

    Fix

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-bash docker-compose run --rm openvpn ovpn_genconfig -u udp://167.172.35.156 
-bash docker-compose run -e EASYRSA_KEY_SIZE=4096 --rm openvpn ovpn_initpki
+docker-compose run --rm openvpn ovpn_genconfig -u udp://167.172.35.156 
+docker-compose run -e EASYRSA_KEY_SIZE=4096 --rm openvpn ovpn_initpki
