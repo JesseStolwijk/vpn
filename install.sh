936a09d06bcf551c3210bddd57dbabec819b29de
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-01-25 18:21:32 +0100

    Create docker compose for pihole + openvpn

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose run --rm openvpn ovpn_genconfig -u udp://167.172.35.156 
+docker-compose run -e EASYRSA_KEY_SIZE=4096 --rm openvpn ovpn_initpki
+sudo chown -R $(whoami): ./openvpn-data
