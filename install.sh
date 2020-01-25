c59cec73f31287169639e4dc1ab0b108b82c9aa8
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-01-25 22:16:51 +0100

    remove containerd.io

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install -y docker-ce docker-ce-cli containerd.io
+yum install -y docker-ce docker-ce-cli
 
 systemctl start docker
 
