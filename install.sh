6b8000265a516b024cc21e3f8b2b812fffb905b6
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-01-25 21:34:42 +0100

    Create network

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker network create --driver=bridge --subnet=192.168.254.0/24 --gateway=192.168.254.1 vpn-network
+
 docker-compose run --rm openvpn ovpn_genconfig -u udp://167.172.35.156 -n 192.168.254.11
 docker-compose run --rm openvpn ovpn_initpki
